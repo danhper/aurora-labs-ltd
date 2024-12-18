e08b6d7bf93d37e041ac91041e0007e54381e3e6
Chase Manning <chase@manning.dev>
2024-12-18 14:03:58 +0000

    :lipstick: restyle our projects

--- src/pages/landing/OurProjects.tsx
+++ src/pages/landing/OurProjects.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import styled from "styled-components";
-import Project from "../../components/Project";
-import bg from "../../assets/backgrounds/5C.png";
 
 import tlx01 from "../../assets/screenshots/tlx/01.png";
 import tlx02 from "../../assets/screenshots/tlx/02.png";
@@ -15,22 +13,13 @@ import mero04 from "../../assets/screenshots/mero/04.png";
 
 const StyledOurProjects = styled.div`
   width: 100%;
-  height: 100dvh;
-  z-index: 1000;
   overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
   flex-direction: column;
-`;
-
-const Background = styled.img`
-  width: 100%;
-  height: 100dvh;
-  position: absolute;
-  top: 0;
-  left: 0;
+  padding: 15rem 0;
 `;
 
 const Content = styled.div`
@@ -38,86 +27,106 @@ const Content = styled.div`
   align-items: center;
   flex-direction: column;
   width: 100%;
-  gap: 7rem;
-`;
-
-const Links = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  mix-blend-mode: difference;
+  max-width: 150rem;
 `;
 
 const Header = styled.h2`
-  font-size: 8vw;
+  font-size: 10rem;
   font-weight: 800;
-  margin-bottom: 1.6rem;
+  margin-bottom: 10rem;
   text-align: center;
   text-transform: uppercase;
   white-space: nowrap;
   color: var(--main);
   position: relative;
-  mix-blend-mode: difference;
 `;
 
-const Link = styled.button`
-  font-size: 8vw;
-  font-weight: 800;
-  text-align: center;
-  margin-bottom: 2rem;
+const Projects = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+`;
+
+const Project = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const ProjectImage = styled.img`
+  width: 100%;
+  border: 1px solid var(--main);
+  overflow: hidden;
+  aspect-ratio: 16 / 9;
+`;
+
+const ProjectInfo = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+`;
+
+const InfoText = styled.p`
+  font-size: 3rem;
+  font-weight: 600;
   color: var(--main);
-  text-decoration: underline;
-  position: relative;
-  text-transform: uppercase;
-  text-align: justify;
-  cursor: pointer;
+`;
+
+const Description = styled.p`
+  font-size: 2rem;
+  font-weight: 500;
+  color: var(--main);
+  text-align: center;
 `;
 
 const WhoWeAre = () => {
-  const [showMero, setShowMero] = useState(false);
-  const [showTlx, setShowTlx] = useState(false);
-
   return (
     <StyledOurProjects>
-      <Background src={bg} />
       <Content>
         <Header>Our Projects</Header>
-        <Links>
-          <Link
-            onClick={() => {
-              setShowMero(true);
-            }}
-          >
-            Mero
-          </Link>
-          <Link
-            onClick={() => {
-              setShowTlx(true);
-            }}
-          >
-            TLX
-          </Link>
-        </Links>
+        <Projects>
+          <Project>
+            <ProjectInfo>
+              <InfoRow>
+                <InfoText>Mero</InfoText>
+              </InfoRow>
+              <InfoRow>
+                <InfoText style={{ textAlign: "right" }}>2021</InfoText>
+              </InfoRow>
+            </ProjectInfo>
+            <ProjectImage src={mero02} />
+            <Description>
+              Mero redefined liquidity, making it reactive, efficient, and
+              unapologetically dynamic in a space that demanded evolution.
+            </Description>
+          </Project>
+          <Project>
+            <ProjectInfo>
+              <InfoRow>
+                <InfoText>TLX</InfoText>
+              </InfoRow>
+              <InfoRow>
+                <InfoText style={{ textAlign: "right" }}>2024</InfoText>
+              </InfoRow>
+            </ProjectInfo>
+            <ProjectImage src={tlx01} />
+            <Description>
+              TLX distilled leverage into simplicity, crafting non-liquidating
+              tokens that quietly rebalanced to keep traders in control.
+            </Description>
+          </Project>
+        </Projects>
       </Content>
-      <Project
-        show={showMero}
-        hide={() => {
-          setShowMero(false);
-        }}
-        name="Mero"
-        content="Mero redefined liquidity, making it reactive, efficient, and unapologetically dynamic in a space that demanded evolution."
-        images={[mero01, mero02, mero03, mero04]}
-      />
-      <Project
-        show={showTlx}
-        hide={() => {
-          setShowTlx(false);
-        }}
-        name="TLX"
-        content="TLX distilled leverage into simplicity, crafting non-liquidating tokens that quietly rebalanced to keep traders in control."
-        images={[tlx01, tlx02, tlx03, tlx04]}
-      />
     </StyledOurProjects>
   );
 };
