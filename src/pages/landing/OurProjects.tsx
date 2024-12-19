4791ded9821f872bea346b61a138b7f663d73ccd
Chase Manning <chase@manning.dev>
2024-12-19 12:31:27 +0000

    :rocket: add project popup images

--- src/pages/landing/OurProjects.tsx
+++ src/pages/landing/OurProjects.tsx
@@ -2,15 +2,16 @@ import styled from "styled-components";
 
 import tlxLogo from "../../assets/logo/tlx.svg";
 import tlx01 from "../../assets/screenshots/tlx/01.png";
-// import tlx02 from "../../assets/screenshots/tlx/02.png";
-// import tlx03 from "../../assets/screenshots/tlx/03.png";
-// import tlx04 from "../../assets/screenshots/tlx/04.png";
+import tlx02 from "../../assets/screenshots/tlx/02.png";
+import tlx03 from "../../assets/screenshots/tlx/03.png";
+import tlx04 from "../../assets/screenshots/tlx/04.png";
 
 import meroLogo from "../../assets/logo/mero.svg";
-// import mero01 from "../../assets/screenshots/mero/01.png";
+import mero01 from "../../assets/screenshots/mero/01.png";
 import mero02 from "../../assets/screenshots/mero/02.png";
-// import mero03 from "../../assets/screenshots/mero/03.png";
-// import mero04 from "../../assets/screenshots/mero/04.png";
+import mero03 from "../../assets/screenshots/mero/03.png";
+import mero04 from "../../assets/screenshots/mero/04.png";
+import Project from "../../components/Project";
 
 const StyledOurProjects = styled.div`
   width: 100%;
@@ -49,63 +50,22 @@ const Projects = styled.div`
   gap: 2rem;
 `;
 
-const Project = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  align-items: center;
-  gap: 2rem;
-`;
-
-const LogoContainer = styled.div`
-  width: 100%;
-`;
-
-const ProjectLogo = styled.img`
-  height: 4.3rem;
-`;
-
-const ProjectImage = styled.img`
-  width: 100%;
-  border: 1px solid var(--main);
-  overflow: hidden;
-  aspect-ratio: 16 / 9;
-`;
-
-const Description = styled.p`
-  font-size: 2rem;
-  font-weight: 500;
-  color: var(--main);
-  text-align: center;
-`;
-
 const WhoWeAre = () => {
   return (
     <StyledOurProjects>
       <Content>
         <Header>Our Projects</Header>
         <Projects>
-          <Project>
-            <LogoContainer>
-              <ProjectLogo src={tlxLogo} />
-            </LogoContainer>
-            <ProjectImage src={mero02} />
-            <Description>
-              Mero redefined liquidity, making it reactive, efficient, and
-              unapologetically dynamic in a space that demanded evolution.
-            </Description>
-          </Project>
-          <Project>
-            <LogoContainer>
-              <ProjectLogo src={meroLogo} />
-            </LogoContainer>
-            <ProjectImage src={tlx01} />
-            <Description>
-              TLX distilled leverage into simplicity, crafting non-liquidating
-              tokens that quietly rebalanced to keep traders in control.
-            </Description>
-          </Project>
+          <Project
+            logo={meroLogo}
+            images={[mero02, mero01, mero03, mero04]}
+            description="Mero redefined liquidity, making it reactive, efficient, and unapologetically dynamic in a space that demanded evolution."
+          />
+          <Project
+            logo={tlxLogo}
+            images={[tlx01, tlx02, tlx03, tlx04]}
+            description="TLX distilled leverage into simplicity, crafting non-liquidating tokens that quietly rebalanced to keep traders in control."
+          />
         </Projects>
       </Content>
     </StyledOurProjects>
